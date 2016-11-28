6244a77ad2ead467e7f3d673d3a1ff9b06066a13
Guilherme Gadelha <guilhermemgadelha@gmail.com>
2016-11-28 00:05:28 -0300

    script4.R

--- script4.R
+++ script4.R
@@ -19,12 +19,16 @@
 # do curso.
 #
 # Pergunta 3:
-# 
+# Como é a distribuição da média do curso de computação ao longo dos perídos?
+# E dos curso de engenharia elétrica e medicina? Existe uma regressão linear que
+# represente bem essa distribuição (p-valor < .05)?
+#   Resposta Esperada: espera-se que a nota em eng. elétrica e computação caiam com
+# o passar dos anos, devido ao aumento da exigência dos cursos. Não deve existir uma
+# relação linear entre essas duas colunas ( Periodo e Media do Curso).
 #
 
 library(dplyr)
 library(tidyr)
-library(ggplot2)
 
 setwd("~/Desktop/Link to AD2/analytics2/")
 
@@ -32,6 +36,8 @@ df4 <- read.csv("tarefa2/alunosUFCGAnon.csv")
 
 df_test4 <- df4[sample(1:nrow(df4), 10000), ]
 
+# ----------------------------------------------------------------------
+# Perguntas 1 e 2
 
 by_curso <- df4 %>%
   group_by(Nome_Curso) %>%
@@ -62,5 +68,69 @@ barplot(cinco_maiores_evasao$taxaEvasao, main="Cinco Maiores Taxas de Evasão", 
 plot(by_curso$mediaCurso, by_curso$taxaEvasao)
 
 
+# ---------------------------------------------------------------------------
+# Resposta Pergunta 3 - a)
 
+media_comp_by_periodo <- df4 %>%
+  group_by(Nome_Curso) %>%
+  filter(Nome_Curso == 'CIÊNCIA DA COMPUTAÇÃO - D') %>%
+  group_by(Periodo) %>%
+  drop_na(Media_Disciplina) %>%
+  summarise(mediaCurso = mean(Media_Disciplina))
+
+print(media_comp_by_periodo)
+
+plot(media_comp_by_periodo, xlab = 'Periodo', ylab='Media', main='Media de CC vs Periodo')
+
+modelCC <- lm(media_comp_by_periodo$Periodo ~ media_comp_by_periodo$mediaCurso)
+summary(modelCC)
+plot(modelCC)
+
+# como os plots demonstram (qq-plot, plot de residuos e plot do modelo linear)
+# existe uma relação linear alta entre periodo e média do curso naquele perído
+# em ciência da computação. (p-valor = 7.07e-07)
+
+# ---------------------------------------------------------------------------
+# Resposta Pergunta 3 - b)
+
+media_ee_by_periodo <- df4 %>%
+  group_by(Nome_Curso) %>%
+  filter(Nome_Curso == 'ENGENHARIA ELÉTRICA') %>%
+  group_by(Periodo) %>%
+  drop_na(Media_Disciplina) %>%
+  summarise(mediaCurso = mean(Media_Disciplina))
+
+print(media_ee_by_periodo)
+
+plot(media_ee_by_periodo, xlab = 'Periodo', ylab='Media', main='Media de EE vs Periodo')
+
+modelEE <- lm(media_ee_by_periodo$Periodo ~ media_ee_by_periodo$mediaCurso)
+summary(modelEE)
+plot(modelEE)
+
+# como os plots demonstram (qq-plot, plot de residuos e plot do modelo linear)
+# existe uma relação linear alta entre periodo e média do curso naquele perído
+# em eng. elétrica. (p-valor = 0.005145)
+
+# ---------------------------------------------------------------------------
+# Resposta Pergunta 3 - c)
+
+media_med_by_periodo <- df4 %>%
+  group_by(Nome_Curso) %>%
+  filter(Nome_Curso == 'MEDICINA - D') %>%
+  group_by(Periodo) %>%
+  drop_na(Media_Disciplina) %>%
+  summarise(mediaCurso = mean(Media_Disciplina))
+
+print(media_med_by_periodo)
+
+plot(media_med_by_periodo, xlab = 'Periodo', ylab='Media', main='Media de Medicina vs Periodo')
+
+modelMED <- lm(media_med_by_periodo$Periodo ~ media_med_by_periodo$mediaCurso)
+summary(modelMED)
+plot(modelMED)
+
+# como os plots demonstram (qq-plot, plot de residuos e plot do modelo linear)
+# existe uma relação linear alta entre periodo e média do curso naquele perído
+# em medicina. (p-valor = 2.272e-05)
 
