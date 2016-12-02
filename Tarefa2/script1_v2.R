136e06e99aac4d206cb143bc06bfd26c48eed53a
Guilherme Gadelha <guilhermemgadelha@gmail.com>
2016-12-01 22:38:22 -0300

    script1 fix on clean data script

--- Tarefa2/script1_v2.R
+++ Tarefa2/script1_v2.R
@@ -17,15 +17,17 @@
 # f) Use o modelo para predizer o seu próprio desempenho e compare a predição 
 #       com o seu CRA atual. Comente o resultado.
 
-library(dplyr)
-library(tidyr)
-library(plyr)
-
 setwd('~/Desktop/Link to AD2/analytics2/')
 
 df <- read.csv('Tarefa2/graduados_model.csv', sep=' ')  
 
 head(df)
+View(df)
+
+# library(outliers)
+# 
+# outlier(df[2:ncol(df)])
+# rm.outlier(df[2:ncol(df)])
 
 # -----------------------------------------------------------------------------------
 
@@ -47,11 +49,14 @@ summary(model.p1.p2)
 #       a variação em y? Em que grau?
 
 #         Segundo se observa no summary do modelo acima, o p-valor é bastante baixo,
-#        menor que 0.05 (ou 5%), e portanto, o modelo é aceitável.
+#        menor que 0.05 (p-valor = 3.537e-14), e portanto, o modelo é aceitável.
 #         Apesar disso, vemos também que o Adjusted R-squared é baixo, o que significa
 #        que devemos buscar eliminar variáveis que devem estar atrapalhando a 
 #        predição.
+#         Pelo Adjusted R-squared, observa-se que pouco mais da metade da variação é explicada pelo
+#        modelo. Adjusted R-squared = 0.6393
 
+plot(model.p1.p2)
 
 #coefficients(model.p1.p2)
 #confint(model.p1.p2, level=.95)
@@ -93,16 +98,25 @@ plot(final_model)
 # b) Todas as variáveis são úteis para o modelo de regressão?
 #         Não, após aplicar o método de seleção de modelos baseado no AIC (Akaike 
 #       information criterion), chegou-se ao seguinte modelo:
-#             cra = 1.067e-15 * Programação.II + -1.094e-15 * Matemática.Discreta + 7.24
-#         Como se observa, apenas duas disciplinas são realmente significativas
-#       para previsão do cra, considerando apenas conjunto das disciplinas do 1o e 2os
+#             cra = 0.09881 * Álgebra.Vetorial.e.Geometria.Analítica + 
+#                   0.10241 * Leitura.e.Produção.de.Textos + 
+#                   0.30538 * Programação.II +
+#                   0.21886 * Matemática.Discreta +
+#                   0.09098 * Teoria.dos.Grafos
+#                   1.15308
+#
+#         Como se observa, apenas cinco disciplinas são realmente significativas
+#       para previsão do cra, considerando apenas conjunto das disciplinas do 1o e do 2o
 #       periodos.
 #         O modelo tem as seguintes medidas:
-#           p-valor = 1.935e-13
-#           F-statistic = 42.52
-#           Adjusted R-squared = 0.4952
+#           p-value: < 2.2e-16
+#           F-statistic: 33.66
+#           Adjusted R-squared:  0.6577
+#
+#         Nota-se, portanto, uma melhora no nível de explicação da variabilidade que pode ser
+#       explicada pelo modelo proposto.
 
-# -----------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------
 summary(model.p1.p2)
 summary(final_model)
 
@@ -112,20 +126,23 @@ summary(final_model)
 #     Modelo Final encontrado:
 #       cra = 1.067e-15 * Programação.II + -1.094e-15 * Matemática.Discreta + 7.24
 #     Comparação com modelo com todas as variáveis (model.p1.p2):
-#       model             R2        R2-Adj        RSE
-#       model.p1.p2     0.4966      0.4138        4.365e-15
-#       final_model     0.5061      0.4942        4.203e-15
+#       model           p-valor       R2          R2-Adj        RSE
+#       model.p1.p2     3.537e-14     0.6902      0.6393        0.52
+#       final_model     < 2.2e-16     0.6778      0.6577        0.5065                      
 #
 #     Como se pode observar,o modelo final é superior ao modelo com todas as variáveis
 #     em todas as medidas exploradas.
 
 
-# -----------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+
+plot(model.p1.p2)
+plot(final_model)
 
 # d) Analise os plots de resíduos de cada variável e veja se algum (um ou mais) 
 #       deles indica não aleatoriedade dos erros.
 
-# -----------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 
 model.p1 <- lm(cra ~ Cálculo.Diferencial.e.Integral.I +
                     Álgebra.Vetorial.e.Geometria.Analítica +
@@ -161,7 +178,7 @@ summary(model.p2)
 #     Portanto, o modelo 1 descreve/prever melhor o cra do aluno para o curso como
 #     um todo.
 
-# -----------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 
 # f) Use o modelo para predizer o seu próprio desempenho e compare a predição 
 #       com o seu CRA atual. Comente o resultado.
