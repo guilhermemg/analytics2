9a71112f4c9e5050494d62558001fb14ebcd9e55
Guilherme Monteiro Gadelha 21011073 <guilhermemg@lcc1-10.lcc.ufcg.edu.br>
2016-12-12 09:46:37 -0300

    Tarefa2, questoes 8 e 9

--- Tarefa2/Parte 3/script3_1.R
+++ Tarefa2/Parte 3/script3_1.R
@@ -1,14 +1,15 @@
 
 # script 3_1 tarefa 2
 
-setwd('~/Desktop/Link to AD2/analytics2/Tarefa2')
+#setwd('~/Desktop/Link to AD2/analytics2/Tarefa2')
+setwd('~/workspaces/analytics2/Tarefa2')
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------
 #  1. Baixe os dados de treino e teste.
 
 graduados.treino <- read.csv('Parte 3/graduados_treino_model2.csv', sep=' ')
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------
 # 3. Usando todas as variáveis disponíveis (disciplinas do primeiro e segundo período), 
 #     use validação cruzada (nos dados de treino) para tunar um modelo de regressão
 #     Ridge.
@@ -51,6 +52,8 @@ ridge <- train(cra~., data = adapted.graduados.treino,
 )
 ridge
 
+# lambda = 0.2154435
+
 
 graduados.teste <- read.csv('Parte 3/graduados_teste_model2.csv', sep=' ')
 
@@ -76,7 +79,9 @@ ridge.pred <- predict(ridge, adapted.graduados.teste)
 
 sqrt(mean(ridge.pred - adapted.graduados.teste$cra)^2)
 
-# -----------------------------------------------------------------------
+# RSME = 0.8564798
+
+# ----------------------------------------------------------------------------------------------------------------------
 # 4. Mesmo que o item acima mas usando um modelo de regressão Lasso.
 
 
@@ -86,17 +91,24 @@ lasso <- train(cra ~., adapted.graduados.treino,
                trControl=fitControl)
 lasso
 
+# lambda = 0.9
+
 predict.enet(lasso$finalModel, type='coefficients', s=lasso$bestTune$fraction, 
              mode='fraction')
 
 lasso.pred <- predict(lasso, adapted.graduados.teste)
 sqrt(mean(lasso.pred - adapted.graduados.teste$cra)^2)
 
-plot(varImp(lasso))
-plot(varImp(ridge))
+# RSME = 0.7695863
 
-# -----------------------------------------------------------------------
-# 5. Compare os dois modelos nos dados de teste em termos de RMSE.
+varImp(lasso)
+varImp(ridge)
+
+plot(varImp(lasso, scale = FALSE))
+plot(varImp(ridge, scale = FALSE))
+
+# ---------------------------------------------------------------------------------------------------------------------
+# 5. Compare os três modelos nos dados de teste em termos de RMSE.
 # 
 #  RMSE (ridge) = 0.8564798
 #  RMSE (lasso) = 0.7695863
@@ -105,7 +117,7 @@ plot(varImp(ridge))
 #  regressão que utiliza o método de Lasso.
 
 
-# -------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------
 # 6. Quais as variáveis mais importantes segundo o modelo de regressão Lasso? 
 #    Alguma variável foi descartada? Quais?
 #
@@ -117,7 +129,42 @@ plot(varImp(ridge))
 #     . Programação.I
 #
 
-# -------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------------
+# 6.1 Treino com regressão linear com as variáveis mais importantes dos dados de treino
+
+var.mais.importantes.treino <- graduados.treino %>%
+  select(Programação.II, 
+         Laboratório.de.Programação.II,
+         Teoria.dos.Grafos,
+         cra)
+#rownames(var.mais.importantes) <- var.mais.importantes$ALU_NOVAMATRICULA
+
+lmfit <- train(cra ~., data = var.mais.importantes.treino,
+               method='lm',
+               trControl = fitControl,
+               preProc=c('scale', 'center'))
+lmfit
+
+# RMSE (linear)(treino) = 0.5214437
+# Rsquared (linear)(treino) = 0.7406672
+
+coef(lmfit$finalModel)
+
+var.mais.importantes.teste <- graduados.teste %>%
+  select(Programação.II, 
+         Laboratório.de.Programação.II,
+         Teoria.dos.Grafos,
+         cra)
+
+lmfit.pred <- predict(lmfit, var.mais.importantes.teste)
+sqrt(mean(lmfit.pred - var.mais.importantes.teste$cra)^2)
+
+# RMSE (linear)(pred - teste) = 0.6198759
+
+plot(varImp(lmfit, scale = FALSE))
+
+# --------------------------------------------------------------------------------------------------------------
 # 7. Re-treine o melhor modelo (dessa vez nos dados de treino sem validação cruzada) 
 #    e reporte o RMSE no teste.
 
@@ -137,17 +184,51 @@ sqrt(mean(ridge2.pred - adapted.graduados.teste$cra)^2)
 #
 
 
-# -------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
 # 8. Use o modelo treinado em 6 e aplique nos dados de teste que vamos disponibilizar.
 
+treino.mais.teste <- rbind(adapted.graduados.teste, adapted.graduados.treino)
+
+# Melhor modelo encontrado foi o linear: lmfit
+
+lmfit.pred <- predict(lmfit, treino.mais.teste)
+
+sqrt(mean(lmfit.pred - treino.mais.teste$cra)^2)
+
+# RMSE = 0.3333295
 
 
-
-
-# -------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
 # 9. Crie novos atributos a partir dos existentes para tentar melhorar o seu modelo.
 
+novo.treino <- adapted.graduados.treino %>% 
+  dplyr::select(-Fundamentos.de.Física.Clássica, -Leitura.e.Produção.de.Textos, -Cálculo.Diferencial.e.Integral.I) %>%
+  dplyr::mutate(media_labs = (Laboratório.de.Programação.I + Laboratório.de.Programação.II)/2) %>%
+  dplyr::mutate(media_calc = (Cálculo.Diferencial.e.Integral.II + Álgebra.Vetorial.e.Geometria.Analítica)/2) %>%
+  dplyr::mutate(media_prog = (Programação.I + Programação.II)/2)
 
 
+novo.teste <- adapted.graduados.teste %>% 
+  dplyr::select(-Fundamentos.de.Física.Clássica, -Leitura.e.Produção.de.Textos, -Cálculo.Diferencial.e.Integral.I) %>%
+  dplyr::mutate(media_labs = (Laboratório.de.Programação.I + Laboratório.de.Programação.II)/2) %>%
+  dplyr::mutate(media_calc = (Cálculo.Diferencial.e.Integral.II + Álgebra.Vetorial.e.Geometria.Analítica)/2) %>%
+  dplyr::mutate(media_prog = (Programação.I + Programação.II)/2)
 
 
+novo.lasso <- train(cra ~., novo.treino,
+               method='lasso',
+               preProc=c('scale','center'),
+               trControl=fitControl)
+novo.lasso
+
+# lambda = 0.9
+
+predict.enet(novo.lasso$finalModel, type='coefficients', s=novo.lasso$bestTune$fraction, 
+             mode='fraction')
+
+novo.lasso.pred <- predict(novo.lasso, novo.teste)
+sqrt(mean(novo.lasso.pred - novo.teste$cra)^2)
+
+# RSME = 0.4819051
+
+varImp(novo.lasso)
